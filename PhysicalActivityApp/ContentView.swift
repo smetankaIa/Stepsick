d6b9489f5971d92cde90ae6a76a9770ca9811202
smetankaIa <alex.gg3200@gmail.com>
2023-12-20 10:48:20 +0700

    Initial Commit

---
+++ PhysicalActivityApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhysicalActivityApp
+//
+//  Created by Алексей Никулин on 20.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhysicalActivityApp/PhysicalActivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicalActivityAppApp.swift
+//  PhysicalActivityApp
+//
+//  Created by Алексей Никулин on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhysicalActivityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
